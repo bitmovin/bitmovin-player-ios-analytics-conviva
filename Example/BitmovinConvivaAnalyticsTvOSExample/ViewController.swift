e6ab11356b56c0755d5d0df6a609615c29b0b846
David Steinacher <david.steinacher@bitmovin.com>
2018-10-10 13:08:44 +0200

    add tvOS example

---
+++ Example/BitmovinConvivaAnalyticsTvOSExample/ViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ViewController.swift
+//  BitmovinConvivaAnalyticsTvOSExample
+//
+//  Created by David Steinacher on 10.10.18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import BitmovinPlayer
+import BitmovinConvivaAnalytics
+
+class ViewController: UIViewController {
+
+    var player: BitmovinPlayer?
+    var playerView: BMPBitmovinPlayerView?
+    var fullScreen: Bool = false
+
+    var convivaAnalytics: ConvivaAnalytics?
+
+    let convivaCustomerKey: String = ""
+    let convivaGatewayString: String = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupBMPlayer()
+    }
+
+    func setupBMPlayer() {
+        // Setup Player
+        player = BitmovinPlayer()
+
+        let convivaConfig = ConvivaConfiguration()
+
+        // only set gatewayUrl in debug mode !!!
+        convivaConfig.gatewayUrl = URL(string: convivaGatewayString)!
+        convivaConfig.debugLoggingEnabled = true
+
+        convivaConfig.applicationName = "Bitmovin tvOS Conviva integration example app"
+        convivaConfig.viewerId = "awesomeViewerId"
+        convivaConfig.customTags = ["contentType": "Episode"]
+
+        do {
+            convivaAnalytics = try ConvivaAnalytics(player: player!,
+                                                    customerKey: convivaCustomerKey,
+                                                    config: convivaConfig)
+        } catch {
+            NSLog("[ Example ] ConvivaAnalytics initialization failed with error: \(error)")
+        }
+
+        player?.setup(configuration: playerConfiguration)
+
+        // Setup UI
+        playerView = BMPBitmovinPlayerView(player: player!, frame: .zero)
+        playerView?.frame = view.bounds
+
+        if let convivaAnalytics = convivaAnalytics {
+            convivaAnalytics.playerView = playerView
+        }
+
+        playerView?.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        view.addSubview(playerView!)
+        view.bringSubviewToFront(playerView!)
+    }
+
+    var playerConfiguration: PlayerConfiguration {
+        let playerConfiguration = PlayerConfiguration()
+        playerConfiguration.sourceItem = sourceItem
+
+        return playerConfiguration
+    }
+
+    var sourceItem: SourceItem {
+        let sourceString = "https://bitmovin-a.akamaihd.net/content/art-of-motion_drm/m3u8s/11331.m3u8"
+        let sourceItem = SourceItem(url: URL(string: sourceString)!)!
+        sourceItem.posterSource = URL(string: "https://bitmovin-a.akamaihd.net/content/poster/hd/RedBull.jpg")
+        sourceItem.itemTitle = "Art of Motion"
+        return sourceItem
+    }
+}

10d8bb80f7f20528a97824591be38bb6b7db84d9
Alan Allard <alan.r.allard@gmail.com>
2022-05-30 12:44:08 +0200

    All Externally managed session tests passing - except one (needs discussion)

--- Example/Tests/CISVideoAnalyticsTestDouble.swift
+++ Example/Tests/CISVideoAnalyticsTestDouble.swift
@@ -10,7 +10,6 @@ import Foundation
 import ConvivaSDK
 
 class CISVideoAnalyticsTestDouble: NSObject, CISVideoAnalyticsProtocol, TestDoubleDataSource {
-    
     func setContentInfo(_ contentInfo: [AnyHashable: Any]) {
         spy(functionName: "setContentInfo", args: ["contentInfo": "\(String(describing: contentInfo))"])
     }
@@ -20,7 +19,7 @@ class CISVideoAnalyticsTestDouble: NSObject, CISVideoAnalyticsProtocol, TestDoub
     }
 
     func reportPlaybackRequested(_ contentInfo: [AnyHashable: Any]?) {
-        spy(functionName: "reportPlaybackRequested", args: ["contentInfo": "\(String(describing: contentInfo))"])
+        spy(functionName: "reportPlaybackRequested", args: ["assetName": "\(contentInfo?["Conviva.assetName"] ?? "")" ])
     }
 
     func reportPlaybackEnded() {
@@ -33,7 +32,7 @@ class CISVideoAnalyticsTestDouble: NSObject, CISVideoAnalyticsProtocol, TestDoub
     func reportPlaybackError(_ errorMessage: String, errorSeverity severity: ErrorSeverity) {
         spy(functionName: "reportPlaybackError", args: [
             "errorMessage": errorMessage,
-            "errorSeverity": "\(String(describing: severity))"
+            "errorSeverity": "\(severity.rawValue)"
         ])
 
     }
@@ -41,13 +40,12 @@ class CISVideoAnalyticsTestDouble: NSObject, CISVideoAnalyticsProtocol, TestDoub
     func reportPlaybackEvent(_ eventName: String, withAttributes attributes: [AnyHashable: Any]? = nil) {
     }
 
-    func reportAdBreakStarted(_ adPlayer: AdPlayer, adType: AdTechnology, adBreakInfo: [AnyHashable : Any]) {
+    func reportAdBreakStarted(_ adPlayer: AdPlayer, adType: AdTechnology, adBreakInfo: [AnyHashable: Any]) {
         spy(functionName: "reportAdBreakStarted", args: [
             "adPlayer": "\(String(describing: adPlayer))",
             "adType": "\(String(describing: adType))",
             "adBreakInfo": "\(adBreakInfo["c3.ad.position"] ?? "")"
         ])
-        
     }
 
     func reportAdBreakEnded() {
--- Example/Tests/ExternallyManagedSessionTests.swift
+++ Example/Tests/ExternallyManagedSessionTests.swift
@@ -23,195 +23,198 @@ class ExternallyManagedSessionSpec: QuickSpec {
             TestHelper.shared.mockTracker.reset()
         }
 
-//        context("Externally Managed Session") {
-//            var convivaAnalytics: ConvivaAnalytics!
-//            beforeEach {
-//                do {
-//                    convivaAnalytics = try ConvivaAnalytics(player: playerDouble, customerKey: "")
-//                } catch {
-//                    fail("ConvivaAnalytics failed with error: \(error)")
-//                }
-//            }
-//
-//            afterEach {
-//                if convivaAnalytics != nil {
-//                    convivaAnalytics = nil
-//                }
-//            }
-//
-//            context("initialize session") {
-//                var spy: Spy!
-//                beforeEach {
-//                    spy = Spy(aClass: CISClientTestDouble.self, functionName: "createSession")
-//                }
-//                context("without source loaded") {
-//                    beforeEach {
-//                        let playerConfig = PlayerConfig()
-//                        _ = TestDouble(aClass: playerDouble, name: "config", return: playerConfig)
-//                    }
-//
-//                    it("with asset name provided") {
-//                        var metadata: MetadataOverrides = MetadataOverrides()
-//                        metadata.assetName = "MyAsset"
-//                        convivaAnalytics.updateContentMetadata(metadataOverrides: metadata)
-//                        try? convivaAnalytics.initializeSession()
-//                        expect(spy).to(haveBeenCalled(withArgs: ["assetName": "MyAsset"]))
-//                    }
-//
-//                    #if targetEnvironment(simulator)
-//                    // This test will only run on a simulator
-//                    // https://github.com/Quick/Nimble#swift-assertions
+        context("Externally Managed Session") {
+            var convivaAnalytics: ConvivaAnalytics!
+            beforeEach {
+                do {
+                    convivaAnalytics = try ConvivaAnalytics(player: playerDouble, customerKey: "")
+                } catch {
+                    fail("ConvivaAnalytics failed with error: \(error)")
+                }
+            }
+
+            afterEach {
+                if convivaAnalytics != nil {
+                    convivaAnalytics = nil
+                }
+            }
+
+            context("initialize session") {
+                var spy: Spy!
+                beforeEach {
+                    spy = Spy(aClass: CISVideoAnalyticsTestDouble.self, functionName: "reportPlaybackRequested")
+                }
+                context("without source loaded") {
+                    beforeEach {
+                        let playerConfig = PlayerConfig()
+                        _ = TestDouble(aClass: playerDouble, name: "config", return: playerConfig)
+                    }
+
+                    it("with asset name provided") {
+                        var metadata: MetadataOverrides = MetadataOverrides()
+                        metadata.assetName = "MyAsset"
+                        convivaAnalytics.updateContentMetadata(metadataOverrides: metadata)
+                        try? convivaAnalytics.initializeSession()
+                        expect(spy).to(haveBeenCalled(withArgs: ["assetName": "MyAsset"]))
+                    }
+
+                    // TODO: Work out why this is needed!
+                    #if targetEnvironment(simulator)
+                    // This test will only run on a simulator
+                    // https://github.com/Quick/Nimble#swift-assertions
 //                    it("throw error without source and asset name") {
 //                        expect { try convivaAnalytics.initializeSession() }.to(throwError())
 //                        expect(spy).toNot(haveBeenCalled())
 //                    }
-//                    #endif
-//                }
-//
-//                context("with source loaded") {
-//                    beforeEach {
-//                        let playerConfig = PlayerConfig()
-//
-//                        _ = TestDouble(aClass: playerDouble, name: "config", return: playerConfig)
-//                    }
-//
-//                    it("uses item title") {
-//                        let sourceConfig = SourceConfig(url: URL(string: "http://a.url")!, type: .hls)
-//                        sourceConfig.title = "MyTitle"
-//                        let source = SourceFactory.create(from: sourceConfig)
-//                        playerDouble.load(source: source)
-//                        try? convivaAnalytics.initializeSession()
-//                        expect(spy).to(haveBeenCalled(withArgs: ["assetName": "MyTitle"]))
-//                    }
-//
-//                    it("uses asset name attribute") {
-//                        var metadata = MetadataOverrides()
-//                        metadata.assetName = "A Override"
-//                        convivaAnalytics.updateContentMetadata(metadataOverrides: metadata)
-//                        let sourceConfig = SourceConfig(url: URL(string: "http://a.url")!, type: .hls)
-//                        let source = SourceFactory.create(from: sourceConfig)
-//                        playerDouble.load(source: source)
-//
-//                        try? convivaAnalytics.initializeSession()
-//                        expect(spy).to(haveBeenCalled(withArgs: ["assetName": "A Override"]))
-//                    }
-//
-//                    it("throw error without title in the source and without asset name") {
-//                        let sourceConfig = SourceConfig(url: URL(string: "http://a.url")!, type: .hls)
-//                        let source = SourceFactory.create(from: sourceConfig)
-//                        playerDouble.load(source: source)
-//                        expect { try convivaAnalytics.initializeSession() }.to(throwError())
-//                        expect(spy).toNot(haveBeenCalled())
-//                    }
-//                }
-//
-//                it("no-opt if session is running") {
-//                    playerDouble.fakePlayEvent()
-//                    TestHelper.shared.spyTracker.reset()
-//                    try? convivaAnalytics.initializeSession()
-//                    expect(spy).toNot(haveBeenCalled())
-//                }
-//            }
-//
-//            context("end session") {
-//                var spy: Spy!
-//                beforeEach {
-//                    spy = Spy(aClass: CISClientTestDouble.self, functionName: "cleanupSession")
-//                }
-//
-//                it("no-opt if no session running") {
-//                    convivaAnalytics.endSession()
-//                    expect(spy).toNot(haveBeenCalled())
-//                }
-//
-//                it("cleanup running session") {
-//                    playerDouble.fakePlayEvent()
-//                    convivaAnalytics.endSession()
-//                    expect(spy).to(haveBeenCalled())
-//                }
-//            }
-//
-//            context("multiple sessions") {
-//                var spy: Spy!
-//                beforeEach {
-//                    spy = Spy(aClass: CISClientTestDouble.self, functionName: "createSession")
-//                }
-//
-//                it("take the asset name from the source in a consecutive session") {
-//                    // No source at first run
-//                    let playerConfig = PlayerConfig()
-//                    _ = TestDouble(aClass: playerDouble, name: "config", return: playerConfig)
-//
-//                    var metadata = MetadataOverrides()
-//                    metadata.assetName = "MyAsset"
-//                    convivaAnalytics.updateContentMetadata(metadataOverrides: metadata)
-//
-//                    try? convivaAnalytics.initializeSession()
-//                    expect(spy).to(haveBeenCalled(withArgs: ["assetName": "MyAsset"]))
-//                    convivaAnalytics.endSession()
-//
-//                    // With source at second run
-//                    let sourceConfig = SourceConfig(url: URL(string: "http://a.url")!, type: .hls)
-//                    sourceConfig.title = "MyTitle"
-//                    let source = SourceFactory.create(from: sourceConfig)
-//                    playerDouble.load(source: source)
-//                    try? convivaAnalytics.initializeSession()
-//                    expect(spy).to(haveBeenCalled(withArgs: ["assetName": "MyTitle"]))
-//                }
-//            }
-//
-//            context("report playback deficiency") {
-//                var spy: Spy!
-//
-//                beforeEach {
-//                    spy = Spy(aClass: CISClientTestDouble.self, functionName: "reportError")
-//                }
-//
-//                it("no-opt if no session is running") {
-//                    convivaAnalytics.reportPlaybackDeficiency(message: "Test", severity: .ERROR_FATAL)
-//                    expect(spy).toNot(haveBeenCalled())
-//                }
-//
-//                context("reports a deficiency") {
-//                    beforeEach {
-//                        playerDouble.fakePlayEvent()
-//                    }
-//
-//                    it("reports a warning") {
-//                        let severity = ErrorSeverity.ERROR_WARNING
-//                        convivaAnalytics.reportPlaybackDeficiency(message: "Test", severity: .ERROR_WARNING)
-//                        expect(spy).to(haveBeenCalled(withArgs: ["severity": "\(severity.rawValue)"]))
-//                    }
-//
-//                    it("reports an error") {
-//                        let severity = ErrorSeverity.ERROR_FATAL
-//                        convivaAnalytics.reportPlaybackDeficiency(message: "Test", severity: .ERROR_FATAL)
-//                        expect(spy).to(haveBeenCalled(withArgs: ["severity": "\(severity.rawValue)"]))
-//                    }
-//
-//                    context("session closing handling") {
-//                        beforeEach {
-//                            spy = Spy(aClass: CISClientTestDouble.self, functionName: "cleanupSession")
-//                        }
-//                        it("closes session by default") {
-//                            convivaAnalytics.reportPlaybackDeficiency(message: "Test", severity: .ERROR_FATAL)
-//                            expect(spy).to(haveBeenCalled())
-//                        }
-//
-//                        it("closes session if set to true") {
-//                            convivaAnalytics.reportPlaybackDeficiency(message: "Test", severity: .ERROR_FATAL)
-//                            expect(spy).to(haveBeenCalled())
-//                        }
-//
-//                        it("not closes session if set to false") {
-//                            convivaAnalytics.reportPlaybackDeficiency(message: "Test",
-//                                                                      severity: .ERROR_FATAL,
-//                                                                      endSession: false)
-//                            expect(spy).toNot(haveBeenCalled())
-//                        }
-//                    }
-//                }
-//            }
-//        }
+                    #endif
+                }
+
+                context("with source loaded") {
+                    beforeEach {
+                        let playerConfig = PlayerConfig()
+
+                        _ = TestDouble(aClass: playerDouble, name: "config", return: playerConfig)
+                    }
+
+                    it("uses item title") {
+                        let sourceConfig = SourceConfig(url: URL(string: "http://a.url")!, type: .hls)
+                        sourceConfig.title = "MyTitle"
+                        let source = SourceFactory.create(from: sourceConfig)
+                        playerDouble.load(source: source)
+                        try? convivaAnalytics.initializeSession()
+                        expect(spy).to(haveBeenCalled(withArgs: ["assetName": "MyTitle"]))
+                    }
+
+                    it("uses asset name attribute") {
+                        var metadata = MetadataOverrides()
+                        metadata.assetName = "A Override"
+                        convivaAnalytics.updateContentMetadata(metadataOverrides: metadata)
+                        let sourceConfig = SourceConfig(url: URL(string: "http://a.url")!, type: .hls)
+                        let source = SourceFactory.create(from: sourceConfig)
+                        playerDouble.load(source: source)
+
+                        try? convivaAnalytics.initializeSession()
+                        expect(spy).to(haveBeenCalled(withArgs: ["assetName": "A Override"]))
+                    }
+
+                    it("throw error without title in the source and without asset name") {
+                        let sourceConfig = SourceConfig(url: URL(string: "http://a.url")!, type: .hls)
+                        let source = SourceFactory.create(from: sourceConfig)
+                        playerDouble.load(source: source)
+                        expect { try convivaAnalytics.initializeSession() }.to(throwError())
+                        expect(spy).toNot(haveBeenCalled())
+                    }
+                }
+
+                it("no-opt if session is running") {
+                    playerDouble.fakePlayEvent()
+                    TestHelper.shared.spyTracker.reset()
+                    try? convivaAnalytics.initializeSession()
+                    expect(spy).toNot(haveBeenCalled())
+                }
+            }
+
+            context("end session") {
+                var spy: Spy!
+                beforeEach {
+                    spy = Spy(aClass: CISVideoAnalyticsTestDouble.self, functionName: "reportPlaybackEnded")
+                }
+
+                it("no-opt if no session running") {
+                    convivaAnalytics.endSession()
+                    expect(spy).toNot(haveBeenCalled())
+                }
+
+                it("cleanup running session") {
+                    playerDouble.fakePlayEvent()
+                    convivaAnalytics.endSession()
+                    expect(spy).to(haveBeenCalled())
+                }
+            }
+
+            context("multiple sessions") {
+                var spy: Spy!
+                beforeEach {
+                    spy = Spy(aClass: CISVideoAnalyticsTestDouble.self, functionName: "reportPlaybackRequested")
+                }
+
+                it("take the asset name from the source in a consecutive session") {
+                    // No source at first run
+                    let playerConfig = PlayerConfig()
+                    _ = TestDouble(aClass: playerDouble, name: "config", return: playerConfig)
+
+                    var metadata = MetadataOverrides()
+                    metadata.assetName = "MyAsset"
+                    convivaAnalytics.updateContentMetadata(metadataOverrides: metadata)
+
+                    try? convivaAnalytics.initializeSession()
+                    expect(spy).to(haveBeenCalled(withArgs: ["assetName": "MyAsset"]))
+                    convivaAnalytics.endSession()
+
+                    // With source at second run
+                    let sourceConfig = SourceConfig(url: URL(string: "http://a.url")!, type: .hls)
+                    sourceConfig.title = "MyTitle"
+                    let source = SourceFactory.create(from: sourceConfig)
+                    playerDouble.load(source: source)
+                    try? convivaAnalytics.initializeSession()
+                    expect(spy).to(haveBeenCalled(withArgs: ["assetName": "MyTitle"]))
+                }
+            }
+
+            context("report playback deficiency") {
+                var spy: Spy!
+
+                beforeEach {
+                    spy = Spy(aClass: CISVideoAnalyticsTestDouble.self, functionName: "reportPlaybackError")
+
+                }
+
+                it("no-opt if no session is running") {
+                    convivaAnalytics.reportPlaybackDeficiency(message: "Test", severity: .ERROR_FATAL)
+                    expect(spy).toNot(haveBeenCalled())
+                }
+
+                context("reports a deficiency") {
+                    beforeEach {
+                        playerDouble.fakePlayEvent()
+                    }
+
+                    it("reports a warning") {
+                        let severity = ErrorSeverity.ERROR_WARNING
+                        convivaAnalytics.reportPlaybackDeficiency(message: "Test", severity: .ERROR_WARNING)
+                        expect(spy).to(haveBeenCalled(withArgs: ["errorSeverity": "\(severity.rawValue)"]))
+                    }
+
+                    it("reports an error") {
+                        let severity = ErrorSeverity.ERROR_FATAL
+                        convivaAnalytics.reportPlaybackDeficiency(message: "Test", severity: .ERROR_FATAL)
+                        expect(spy).to(haveBeenCalled(withArgs: ["errorSeverity": "\(severity.rawValue)"]))
+                    }
+
+                    context("session closing handling") {
+                        var sessionEndSpy: Spy!
+                        beforeEach {
+                            sessionEndSpy = Spy(aClass: CISVideoAnalyticsTestDouble.self, functionName: "reportPlaybackEnded")
+                        }
+                        it("closes session by default") {
+                            convivaAnalytics.reportPlaybackDeficiency(message: "Test", severity: .ERROR_FATAL)
+                            expect(sessionEndSpy).to(haveBeenCalled())
+                        }
+
+                        it("closes session if set to true") {
+                            convivaAnalytics.reportPlaybackDeficiency(message: "Test", severity: .ERROR_FATAL)
+                            expect(sessionEndSpy).to(haveBeenCalled())
+                        }
+
+                        it("not closes session if set to false") {
+                            convivaAnalytics.reportPlaybackDeficiency(message: "Test",
+                                                                      severity: .ERROR_FATAL,
+                                                                      endSession: false)
+                            expect(sessionEndSpy).toNot(haveBeenCalled())
+                        }
+                    }
+                }
+            }
+        }
     }
 }

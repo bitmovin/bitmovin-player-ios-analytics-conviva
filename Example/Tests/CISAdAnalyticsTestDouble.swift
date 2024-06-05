136186be111e531dedb7299e45b7046e726c2273
Matthias Tamegger <matthias.tamegger@bitmovin.com>
2024-06-05 08:24:25 +0200

    Implement CISAdAnalyticsTestDouble

--- Example/Tests/CISAdAnalyticsTestDouble.swift
+++ Example/Tests/CISAdAnalyticsTestDouble.swift
@@ -9,48 +9,88 @@
 import ConvivaSDK
 import Foundation
 
-class CISAdAnalyticsTestDouble: NSObject, CISAdAnalyticsProtocol {
+class CISAdAnalyticsTestDouble: NSObject, CISAdAnalyticsProtocol, TestDoubleDataSource {
     func isAirPlaying() -> Bool {
         false
     }
 
     func setAdInfo(_ adInfo: [AnyHashable: Any]) {
+        var args = [String: String]()
+        adInfo.forEach { key, value in args[String(describing: key)] = String(describing: value) }
+        spy(functionName: "setAdInfo", args: args)
     }
 
     func setAdPlayerInfo(_ adPlayerInfo: [AnyHashable: Any]) {
+        var args = [String: String]()
+        adPlayerInfo.forEach { key, value in args[String(describing: key)] = String(describing: value) }
+        spy(functionName: "setAdPlayerInfo", args: args)
     }
 
     func reportAdFailed(_ errorMessage: String, adInfo: [AnyHashable: Any]?) {
+        var args = [String: String]()
+        args["errorMessage"] = errorMessage
+        adInfo?.forEach { key, value in args[String(describing: key)] = String(describing: value) }
+        spy(functionName: "reportAdFailed", args: args)
     }
 
     func reportAdLoaded(_ adInfo: [AnyHashable: Any]?) {
+        var args = [String: String]()
+        adInfo?.forEach { key, value in args[String(describing: key)] = String(describing: value) }
+        spy(functionName: "reportAdLoaded", args: args)
     }
 
     func reportAdStarted(_ adInfo: [AnyHashable: Any]?) {
+        var args = [String: String]()
+        adInfo?.forEach { key, value in args[String(describing: key)] = String(describing: value) }
+        spy(functionName: "reportAdStarted", args: args)
     }
 
     func reportAdEnded() {
+        spy(functionName: "reportAdEnded")
     }
 
     func reportAdError(_ errorMessage: String, severity: ErrorSeverity) {
+        spy(functionName: "reportAdError", args: [
+            "errorMessage": errorMessage,
+            "severity": "\(severity)"
+        ])
     }
 
     func reportAdSkipped() {
+        spy(functionName: "reportAdSkipped")
     }
 
     func reportAdPlayerEvent(_ eventType: String, details: [AnyHashable: Any]?) {
+        var args = [String: String]()
+        args["eventType"] = eventType
+        details?.forEach { key, value in args[String(describing: key)] = String(describing: value) }
+        spy(functionName: "reportAdPlayerEvent", args: args)
     }
 
     func reportAdMetric(_ key: String, value: Any) {
+        spy(functionName: "reportAdMetric", args: [
+            "key": key,
+            "value": "\(value)"
+        ])
     }
 
     func setContentSessionID(_ sessionID: Int32) {
+        spy(functionName: "setContentSessionID", args: [
+            "sessionID": "\(sessionID)"
+        ])
     }
 
     func setAdListener(_ adProxy: Any?, andInfo info: [AnyHashable: Any]) {
+        var args = [String: String]()
+        args["adProxy"] = "\(adProxy ?? "nil")"
+        info.forEach { key, value in args[String(describing: key)] = String(describing: value) }
+        spy(functionName: "setAdListener", args: args)
     }
 
     func report(_ playerState: PlayerState) {
+        spy(functionName: "report", args: [
+            "playerState": "\(playerState)"
+        ])
     }
 
     func getSessionId() -> Int32 {
@@ -66,6 +106,10 @@ class CISAdAnalyticsTestDouble: NSObject, CISAdAnalyticsProtocol {
     }
 
     func reportPlaybackMetric(_ key: String, value: Any?) {
+        spy(functionName: "reportPlaybackMetric", args: [
+               "key": key,
+               "value": "\(value ?? "nil")"
+           ])
     }
 
     func setUpdateHandler(_ updateHandler: @escaping UpdateHandler) {
@@ -80,5 +124,6 @@ class CISAdAnalyticsTestDouble: NSObject, CISAdAnalyticsProtocol {
     }
 
     func cleanup() {
+        spy(functionName: "cleanup")
     }
 }

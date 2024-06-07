6a7b459fbe01401290513f2ce94e8d488e61fedb
Matthias Tamegger <matthias.tamegger@bitmovin.com>
2024-06-07 10:20:01 +0200

    Keep body length local swiftlint ignore

--- BitmovinConvivaAnalytics/Classes/ConvivaAnalytics.swift
+++ BitmovinConvivaAnalytics/Classes/ConvivaAnalytics.swift
@@ -13,6 +13,7 @@ import Foundation
 
 private let notAvailable = "NA"
 
+// swiftlint:disable:next type_body_length
 public final class ConvivaAnalytics: NSObject {
     // MARK: - Bitmovin Player attributes
     let player: Player

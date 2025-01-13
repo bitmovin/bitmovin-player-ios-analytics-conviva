a35d719bb1fff419d8178ee463cae10d87f966dd
Roland Kakonyi <roland.kakonyi@bitmovin.com>
2025-01-13 15:18:04 +0100

    fix unit test build

--- Example/Tests/Doubles/BitmovinPlayerTestDouble.swift
+++ Example/Tests/Doubles/BitmovinPlayerTestDouble.swift
@@ -70,7 +70,6 @@ class BitmovinPlayerTestDouble: BitmovinPlayerStub, TestDoubleDataSource {
         onPlayerError(
             PlayerErrorEvent(
                 code: PlayerError.Code.networkGeneral,
-                errorCode: 1_200,
                 message: "Test player error",
                 data: nil
             ),
@@ -85,7 +84,6 @@ class BitmovinPlayerTestDouble: BitmovinPlayerStub, TestDoubleDataSource {
         onSourceError(
             SourceErrorEvent(
                 code: SourceError.Code.general,
-                errorCode: 2_001,
                 message: "Test source error",
                 data: nil
             ),

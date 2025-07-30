9c5ce162202a40f102e5cbcf84ad4cd494c081d6
Roland Kakonyi <roland.kakonyi@bitmovin.com>
2025-07-31 00:33:36 +0200

    fix missing StreamType error

---
+++ BitmovinConvivaAnalytics/Classes/StreamType.swift
@@ -0,0 +1,17 @@
+//
+// Bitmovin Player iOS SDK
+// Copyright (C) 2024, Bitmovin GmbH, All Rights Reserved
+//
+// This source code and its use and distribution, is subject to the terms
+// and conditions of the applicable license agreement.
+//
+
+import Foundation
+
+// swiftlint:disable identifier_name
+public enum StreamType: String {
+    case CONVIVA_STREAM_LIVE = "LIVE"
+    case CONVIVA_STREAM_VOD = "VOD"
+    case CONVIVA_STREAM_UNKNOWN = "UNKNOWN"
+}
+// swiftlint:enable identifier_name

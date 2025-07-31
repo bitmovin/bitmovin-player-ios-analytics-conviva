9ed73c20d839a43f682ab6370771d1027baa6487
Roland Kákonyi <roland.kakonyi@bitmovin.com>
2025-07-31 08:51:04 +0200

    Fix date
    Co-authored-by: David Steinacher <david.steinacher@bitmovin.com>

--- BitmovinConvivaAnalytics/Classes/StreamType.swift
+++ BitmovinConvivaAnalytics/Classes/StreamType.swift
@@ -1,6 +1,6 @@
 //
 // Bitmovin Player iOS SDK
-// Copyright (C) 2024, Bitmovin GmbH, All Rights Reserved
+// Copyright (C) 2025, Bitmovin GmbH, All Rights Reserved
 //
 // This source code and its use and distribution, is subject to the terms
 // and conditions of the applicable license agreement.
